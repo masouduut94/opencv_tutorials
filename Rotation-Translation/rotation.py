93132a994ca251ae2b65999c5050d9c15bb01993
masoud <masouduut94@gmail.com>
2022-07-14 03:47:11 -0700

    Added rotation

---
+++ Rotation-Translation/rotation.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("../assets/IMAGES/aventador.png")
+img = cv2.resize(img, None, fx=.5, fy=.5)
+
+simple_rot = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+
+cv2.imshow("original", img)
+cv2.imshow("simple rotate", simple_rot)
+
+h, w, _ = img.shape
+
+rot_Mat = cv2.getRotationMatrix2D((w/2, h/2), 125, 0.5)
+rot_image_output = cv2.warpAffine(img, rot_Mat, (w, h))
+cv2.imshow("rotated", rot_image_output)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
