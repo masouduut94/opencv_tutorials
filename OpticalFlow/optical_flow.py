4f3ce08a2f1341951e7ef44f35f1ce2015114724
masoud <masouduut94@gmail.com>
2022-07-20 10:03:14 -0700

    Added Optical Flow

---
+++ OpticalFlow/optical_flow.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+from time import time
+
+
+def draw_flow(img, flow, step=16):
+    h, w, _ = img.shape
+    x, y = np.meshgrid[step / 2:h:step, step / 2:w:step].reshape(2, -1).astype(int)
+    fx, fy = flow[y, x].T
+
+    lines = np.vstack([x, y, x - fx, y - fy]).T.reshape(-1, 2, 2)
+    lines = np.int32(lines + 0.5)
+
+    img_bgr = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    cv2.polylines(img_bgr, lines, 0, (0, 255, 0))
+
+    for (x1, y1), (_x2, _y2) in lines:
+        cv2.circle(img_bgr, (x1, y1), 1, (0, 255, 0), -1)
+
+    return img_bgr
+
+
+def draw_hsv(flow):
+    h, w, _ = flow.shape
+    fx, fy = flow[:, :, 0], flow[:, :, 1]
+
+    ang = np.arctan2(fy, fx) + np.pi
+    v = np.sqrt(fx*fx + fy*fy)
+
+    hsv = np.zeros((h, w, 3), np.uint8)
+    hsv[..., 0] = ang*(180/np.pi/2)
+    hsv[..., 1] = 255
+    hsv[..., 2] = np.minimum(v*4, 255)
+    bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+
+    return bgr
+
+if __name__ == '__main__':
+    cap = cv2.VideoCapture(0)
+    
+
+
+
+
