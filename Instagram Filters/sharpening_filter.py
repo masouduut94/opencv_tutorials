e70bd50aec11a7e8560522049246aa68629213ae
masoud <masouduut94@gmail.com>
2022-07-21 02:54:29 -0700

    Organize all scripts using Meta Module

--- Instagram Filters/sharpening_filter.py
+++ Instagram Filters/sharpening_filter.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 
+from assets import AssetMeta
+
 
 def apply_sharpening(image):
     """
@@ -26,8 +28,9 @@ def apply_sharpening(image):
 
 
 if __name__ == '__main__':
-    img = cv2.imread("../assets/IMAGES/woman-boat.jpg")
-    img = cv2.resize(img, (0, 0), fx=0.15, fy=0.15)
+    meta = AssetMeta()
+    img = cv2.imread(meta.IMG_NORMAL[0])
+    img = cv2.resize(img, (960, 540))
     new_img = apply_sharpening(img.copy())
     cv2.imshow("result", new_img)
     cv2.imshow("original", img)
