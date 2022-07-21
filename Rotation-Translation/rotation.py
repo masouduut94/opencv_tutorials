441591a00cc2afbcee85130af5f602c153458544
masoud <masouduut94@gmail.com>
2022-07-21 03:13:49 -0700

    Organize all scripts using Meta Module

--- Rotation-Translation/rotation.py
+++ Rotation-Translation/rotation.py
@@ -6,19 +6,23 @@ SRC: https://www.youtube.com/watch?v=yDGWSeI0POw&list=PLlH6o4fAIji6epixHdlBYZthV
 import cv2
 import numpy as np
 
-img = cv2.imread("../assets/IMAGES/aventador.png")
-img = cv2.resize(img, None, fx=.5, fy=.5)
+from assets import AssetMeta
 
-simple_rot = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+if __name__ == '__main__':
+    meta = AssetMeta()
+    img = cv2.imread(meta.IMG_NORMAL[0])
+    img = cv2.resize(img, None, fx=.5, fy=.5)
 
-cv2.imshow("original", img)
-cv2.imshow("simple rotate", simple_rot)
+    simple_rot = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
 
-h, w, _ = img.shape
+    cv2.imshow("original", img)
+    cv2.imshow("simple rotate", simple_rot)
 
-rot_Mat = cv2.getRotationMatrix2D((w/2, h/2), 125, 0.5)
-rot_image_output = cv2.warpAffine(img, rot_Mat, (w, h))
-cv2.imshow("rotated", rot_image_output)
+    h, w, _ = img.shape
 
-cv2.waitKey()
-cv2.destroyAllWindows()
+    rot_Mat = cv2.getRotationMatrix2D((w/2, h/2), 125, 0.5)
+    rot_image_output = cv2.warpAffine(img, rot_Mat, (w, h))
+    cv2.imshow("rotated", rot_image_output)
+
+    cv2.waitKey()
+    cv2.destroyAllWindows()
